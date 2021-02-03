1cf82691d65e691ab2d74d0249123ae031f2002d
Daniel Fuentes <abnerfuentes05@gmail.com>
2021-02-03 09:23:56 -0600

    cambio en el color del tema

--- public/js/app.js
+++ public/js/app.js
@@ -49900,8 +49900,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\2DO PTN IS\desktop\proyecto1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\2DO PTN IS\desktop\proyecto1\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Lenovo\Desktop\laravelProyectos\proyecto1\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Lenovo\Desktop\laravelProyectos\proyecto1\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
