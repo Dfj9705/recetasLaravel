8927b8e02580297872f9e81953864836ff11d756
DANIEL FUENTES <abnerfuentes05@gmail.com>
2021-02-05 09:39:08 -0600

    se creo validacion para campo de titulo de receta

--- resources/views/recetas/create.blade.php
+++ resources/views/recetas/create.blade.php
@@ -6,11 +6,22 @@
     <h2 class="text-center mb-5">Crear nueva receta</h2>
     <div class="row justify-content-center mt-5">
         <div class="col-md-8">
-            <form method="post" action="{{route('recetas.store')}}">
+            <form method="post" action="{{route('recetas.store')}}" novalidate>
                 @csrf
                 <div class="form-group">
                     <label for="titulo">Titulo de la receta</label>
-                    <input type="text" name="titulo" id="titulo" class="form-control"placeholder="Titulo de la receta"/>
+                    <input type="text" 
+                        name="titulo" 
+                        id="titulo" 
+                        class="form-control @error('titulo') is-invalid @enderror"
+                        value="{{ old('titulo') }}"
+                        placeholder="Titulo de la receta"
+                    />
+                    @error('titulo')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <input type="submit" class="btn btn-primary" value="Agregar Receta"/>
