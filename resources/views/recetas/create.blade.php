05241a29e661c2f9d4f636b93a609e8aaee94244
3ptnDesarrollo <afuentesj1@mindef.mil.gt>
2021-02-19 08:44:09 -0600

    se agrego trix editor para ingreso de texto

--- resources/views/recetas/create.blade.php
+++ resources/views/recetas/create.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.app')
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css" integrity="sha512-CWdvnJD7uGtuypLLe5rLU3eUAkbzBR3Bm1SFPEaRfvXXI2v2H5Y0057EMTzNuGGRIznt8+128QIDQ8RqmHbAdg==" crossorigin="anonymous" />
+@endsection
 @section('botones')
     <a href="{{ route('recetas.index') }}" class="btn btn-primary mr-2 text-white">Volver</a>
 @endsection
@@ -46,10 +49,47 @@
                         </span>
                     @enderror
                 </div>
+                <div class="form-group mt-3">
+                    <label for="preparacion">Preparación</label>
+                    <input type="hidden" name="preparacion" value="{{old('preparacion')}}" id="preparacion">
+                    <trix-editor 
+                        input="preparacion"
+                        class="form-control @error('preparacion') is-invalid @enderror"
+                    >
+                    </trix-editor>
+                    @error('preparacion')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group mt-3">
+                    <label for="ingredientes">Ingredientes</label>
+                    <input type="hidden" name="ingredientes" value="{{old('ingredientes')}}" id="ingredientes">
+                    <trix-editor 
+                        input="ingredientes"
+                        class="form-control @error('ingredientes') is-invalid @enderror"    
+                    >
+                    </trix-editor>
+                    @error('ingredientes')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group mt-3">
+                    <label for="imagen">Elige la imagen</label>
+                    <input type="file" name="imagen" id="imagen" class="form-control">
+
+                </div>
                 <div class="form-group">
                     <input type="submit" class="btn btn-primary" value="Agregar Receta"/>
                 </div>
             </form>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js" integrity="sha512-/1nVu72YEESEbcmhE/EvjH/RxTg62EKvYWLG3NdeZibTCuEtW5M4z3aypcvsoZw03FAopi94y04GhuqRU9p+CQ==" crossorigin="anonymous"></script>
 @endsection
