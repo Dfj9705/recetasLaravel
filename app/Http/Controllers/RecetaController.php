25b4a2dd311dc29c7928d419c589f3acc1d784ab
3ptnDesarrollo <afuentesj1@mindef.mil.gt>
2021-02-23 10:23:49 -0600

    se agrego libreria para cortar imagen

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -6,6 +6,7 @@ use App\Receta;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+// use Intervention\Image\Facades\Image;
 
 class RecetaController extends Controller
 {
@@ -59,6 +60,10 @@ class RecetaController extends Controller
         
         //obtencion de la ruta de la imagen
         $ruta_imagen = $request['imagen']->store('upload-recetas','public');
+
+        //resize de la img
+        // $img = Image::make( public_path("storage/{$ruta_imagen}"))->fit(1000,550);
+        // $img->save();
 
         //almacenar en la base de datos
         DB::table('recetas')->insert([
