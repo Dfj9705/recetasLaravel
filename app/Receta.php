5c78d037c5c64d9e5e6c36bf963939b2c307f4bb
Daniel Fuentes <abnerfuentes05@gmail.com>
2021-03-03 18:43:08 -0600

    Se agrego relacion de recetas a categoria

--- app/Receta.php
+++ app/Receta.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Receta extends Model
 {
-    //
+    //obtener la categoria de la receta con FK
+    public function categoria()
+    {
+        return $this->belongsTo(CategoriaReceta::class);
+    }
 }
