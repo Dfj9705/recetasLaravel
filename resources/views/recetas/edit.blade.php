40795be5821850d2edc54b779d436627512e0aee
Daniel Fuentes <abnerfuentes05@gmail.com>
2021-03-15 15:02:48 -0600

    Se creo formulario de edicion

---
+++ resources/views/recetas/edit.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.app')
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css" integrity="sha512-CWdvnJD7uGtuypLLe5rLU3eUAkbzBR3Bm1SFPEaRfvXXI2v2H5Y0057EMTzNuGGRIznt8+128QIDQ8RqmHbAdg==" crossorigin="anonymous" />
+@endsection
+@section('botones')
+    <a href="{{ route('recetas.index') }}" class="btn btn-primary mr-2 text-white">Volver</a>
+@endsection
+@section('content')
+    <h2 class="text-center mb-5">Editar receta: {{ $receta->titulo }}</h2>
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
+            <form method="post" action="{{route('recetas.store')}}" enctype="multipart/form-data" novalidate>
+                @csrf
+                <div class="form-group">
+                    <label for="titulo">Titulo de la receta</label>
+                    <input type="text"
+                        name="titulo"
+                        id="titulo"
+                        class="form-control @error('titulo') is-invalid @enderror"
+                        value="{{ $receta->titulo }}"
+                        placeholder="Titulo de la receta"
+                    />
+                    @error('titulo')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="categoria">Categoria</label>
+                    <select
+                        name="categoria"
+                        id="categoria"
+                        class="form-control @error('categoria') is-invalid @enderror"
+                    >
+                        <option value="">Seleccione...</option>
+                        @foreach($categorias as $categoria)
+                            <option
+                                value="{{ $categoria->id }}"
+                                {{ $receta->categoria_id == $categoria->id ? 'selected' : ''}}
+                            >
+                                {{ $categoria->nombre }}
+                            </option>
+                        @endforeach
+                    </select>
+                    @error('categoria')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group mt-3">
+                    <label for="preparacion">Preparación</label>
+                    <input type="hidden" name="preparacion" value="{{ $receta->preparacion }}" id="preparacion">
+                    <trix-editor
+                        input="preparacion"
+                        class="form-control @error('preparacion') is-invalid @enderror"
+                    >
+                    </trix-editor>
+                    @error('preparacion')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group mt-3">
+                    <label for="ingredientes">Ingredientes</label>
+                    <input type="hidden" name="ingredientes" value="{{ $receta->ingredientes }}" id="ingredientes">
+                    <trix-editor
+                        input="ingredientes"
+                        class="form-control @error('ingredientes') is-invalid @enderror"
+                    >
+                    </trix-editor>
+                    @error('ingredientes')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group mt-3">
+                    <label for="imagen">Elige la imagen</label>
+                    <input type="file" name="imagen" id="imagen" class="form-control @error('imagen') is-invalid @enderror">
+                    <div>
+                        <p>Imagen Actual</p>
+                        <img src="/storage/{{ $receta->imagen }}" style="width: 300px">
+                    </div>
+
+                    @error('imagen')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Agregar Receta"/>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js" integrity="sha512-/1nVu72YEESEbcmhE/EvjH/RxTg62EKvYWLG3NdeZibTCuEtW5M4z3aypcvsoZw03FAopi94y04GhuqRU9p+CQ==" crossorigin="anonymous" defer></script>
+@endsection
