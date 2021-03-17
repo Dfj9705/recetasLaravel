c4741769b6a8565df798f86c843147f86a75ce3d
Daniel Fuentes <abnerfuentes05@gmail.com>
2021-03-17 11:34:19 -0600

    Se creo modelo, contralador y migracion para el perfil del usuario

--- public/js/app.js
+++ public/js/app.js
@@ -76011,8 +76011,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\users\lenovo\desktop\laravelProyectos\proyecto1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\users\lenovo\desktop\laravelProyectos\proyecto1\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Lenovo\Desktop\laravelProyectos\proyecto1\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Lenovo\Desktop\laravelProyectos\proyecto1\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
