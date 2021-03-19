01c265a0a6872f2c7eda1cf99cd12ee6cad2ccb7
Daniel Fuentes <abnerfuentes05@gmail.com>
2021-03-18 21:36:06 -0600

    se obtuvieron las recetas y se mandaron a la vista

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Receta;
 use Illuminate\Http\Request;
 
 class InicioController extends Controller
 {
     public function index()
     {
-        return view('inicio.index');
+        // obteniendo las recetas mas nuevas
+        // $nuevas = Receta::orderBy('created_at','DESC')->get();
+        $nuevas = Receta::latest()->take(5)->get();//esto es lo mismo que arriba
+
+        return view('inicio.index',compact('nuevas'));
     }
 }
