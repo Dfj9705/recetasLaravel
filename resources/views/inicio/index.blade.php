7fd7bba97f157b5df7ef0fd9dbb69c2c6bf05c4e
Daniel Fuentes <abnerfuentes05@gmail.com>
2021-03-26 11:23:54 -0600

    Se agrego owl carrousel a la pagina de inicio de la app

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
 @section('styles')
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" />
 @endsection
 
 @section('content')
     <div class="container nuevas-recetas">
-        <h2 class="titulo-categoria text-uppercase mt-5 mb-4">Últimas Recetas</h2>
-        <div class="row">
+        <h2 class="titulo-categoria text-uppercase mb-4">Últimas Recetas</h2>
+        <div class="owl-carousel owl-theme">
             @foreach ($nuevas as $nueva)
-            <div class="col-md-4">
+
                 <div class="card">
                     <img src="/storage/{{ $nueva->imagen }}" alt="imagen receta" class="card-img-top">
                     <div class="card-body">
@@ -23,7 +23,7 @@
                         </a>
                     </div>
                 </div>
-            </div>
+
             @endforeach
         </div>
     </div>
