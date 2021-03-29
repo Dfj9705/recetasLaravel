ca3abe6b27166937116335c35d0f7b71bdab3288
3ptnDesarrollo <afuentesj1@mindef.mil.gt>
2021-03-29 15:50:36 -0600

    se agrego mas informacion en la receta que se muestra al inicio

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -40,6 +40,18 @@
                                 <img src="/storage/{{ $receta->imagen }}" alt="iamgen receta" class="card-img-top">
                                 <div class="card-body">
                                     <h3 class="card-title">{{ $receta->titulo}}</h3>
+                                    <div class="meta-receta d-flex justify-content-between">
+                                        @php
+                                            $fecha = $receta->created_at
+                                        @endphp
+                                        <p class="text-primary fecha font-weight-bold">
+                                            <fecha-receta fecha="{{$fecha}}"></fecha-receta>
+
+                                        </p>
+                                        <p>{{ count($receta->likes) }} Les gustó</p>
+                                    </div>
+                                    <p class="card-text">{{ Str::words( strip_tags($nueva->preparacion) , 20 ) }}</p>
+                                    <a href="{{ route('recetas.show', ['receta' => $receta->id ]) }}" class="btn btn-primary d-block btn-receta">Ver receta</a>
                                 </div>
                             </div>
                         </div>
