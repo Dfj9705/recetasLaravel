412fd2ab5b5dca37d8becab17ee7c2c87709f3d5
Daniel Fuentes <abnerfuentes05@gmail.com>
2021-04-04 15:29:48 -0600

    no se pudo, pagar un plan

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -74,7 +74,7 @@ class RecetaController extends Controller
 
         //obtencion de la ruta de la imagen
         $ruta_imagen = $request['imagen']->store('upload-recetas','public');
-            return public_path("storage/".$ruta_imagen);
+            // return public_path("storage/".$ruta_imagen);
 
         //resize de la img
         $img = Image::make( public_path("storage/{$ruta_imagen}"))->fit(1000,550);
